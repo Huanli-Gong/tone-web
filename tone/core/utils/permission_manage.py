0b7f08e7cc16a211f00f5c05d264d8e801439b4c
张学峰 <wb-zxf699644@alibaba-inc.com>
2023-06-21 15:06:12 +0800

    fix: add ws_member for query job

--- tone/core/utils/permission_manage.py
+++ tone/core/utils/permission_manage.py
@@ -61,7 +61,7 @@ def check_job_operator_permission(user_name, check_obj):
     if sys_role not in ['super_admin', 'sys_admin']:
         operator_role_id = WorkspaceMember.objects.get(ws_id=check_obj.ws_id, user_id=user.id).role_id
         operator_role = Role.objects.get(id=operator_role_id).title
-        allow_list = ['ws_owner', 'ws_admin', 'ws_test_admin']
+        allow_list = ['ws_owner', 'ws_admin', 'ws_test_admin', 'ws_member']
         if operator_role not in allow_list and user.id != check_obj.creator:
             return False
     return True
