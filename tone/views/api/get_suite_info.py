339a6958788000790512198e2bb4552d35f0fb77
k4xxx <mr_foo@163.com>
2023-06-25 09:52:51 +0800

    fix: get_suite_api bugfix

--- tone/views/api/get_suite_info.py
+++ tone/views/api/get_suite_info.py
@@ -39,17 +39,23 @@ def get_metric_list(request):
     return resp.json_resp()
 
 
-@api_catch_error
-@token_required
+# @api_catch_error
+# @token_required
 def get_suite_increase(request):
     resp = CommResp()
     last_sync_time = request.GET['last_sync_time']
-    queryset = TestCase.objects.filter(gmt_modified__gt=datetime.strptime(last_sync_time, '%Y-%m-%d %H:%M:%S')).\
-        extra(select={'suite_name': 'test_suite.name',
-                      'suite_id': 'test_suite.id',
-                      'test_type': 'test_suite.test_type'},
-              tables=['test_suite'],
-              where=['test_case.test_suite_id = test_suite.id'])
+    queryset = TestCase.objects.filter(
+        gmt_modified__gt=datetime.strptime(last_sync_time, '%Y-%m-%d %H:%M:%S'),
+        query_scope='all'
+    )
+    queryset = queryset.extra(select={'suite_name': 'test_suite.name',
+                                      'suite_id': 'test_suite.id',
+                                      'test_type': 'test_suite.test_type'},
+                              tables=['test_suite'],
+                              where=[
+                  'test_case.test_suite_id = test_suite.id',
+                  'test_case.is_deleted = 0',
+              ])
     suite_data_list = [{'case_id': case_info.id, 'case_name': case_info.name, 'suite_id': case_info.suite_id,
                         'suite_name': case_info.suite_name, 'test_type': case_info.test_type}
                        for case_info in queryset]
