3ed5750faf480375f156d0856b570c7e819d4af1
张学峰 <wb-zxf699644@alibaba-inc.com>
2023-08-23 11:13:44 +0800

    feature: add template detail for report detail api to front

--- tone/core/handle/report_handle.py
+++ tone/core/handle/report_handle.py
@@ -10,7 +10,7 @@ import json
 from datetime import datetime
 import logging
 from tone.core.utils.common_utils import query_all_dict
-
+from django.forms.models import model_to_dict
 from django.db import transaction
 
 from tone.models import TestJob, ReportTemplate, Report, TestServerSnapshot, CloudServerSnapshot, ReportItemConf, \
@@ -820,3 +820,300 @@ def get_group_server_info(base_group, compare_groups):
         else:
             env_info['compare_groups'].append(group_env_info)
     return env_info
+
+
+def get_report_template(template_id):
+    template_info = dict()
+    template = ReportTemplate.objects.filter(id=template_id).first()
+    if template:
+        template_info = model_to_dict(template)
+        template_info['func_item'] = get_template_func_item(template.is_default, template_id)
+        template_info['perf_item'] = get_template_perf_item(template.is_default, template_id)
+        template_info['func_conf'] = get_template_conf(template_id, test_type='functional')
+        template_info['perf_conf'] = get_template_conf(template_id, test_type='performance')
+    return template_info
+
+
+def get_conf(tmpl_id, test_type):
+    item_id_list = ReportTmplItem.objects.filter(tmpl_id=tmpl_id, test_type=test_type).values_list('id')
+    item_suite = ReportTmplItemSuite.objects.filter(report_tmpl_item_id__in=item_id_list).first()
+    if item_suite is not None:
+        res = {
+            'need_test_description': item_suite.need_test_description,
+            'test_description_desc': item_suite.test_description_desc,
+            'need_test_conclusion': item_suite.need_test_conclusion,
+            'test_conclusion_desc': item_suite.test_conclusion_desc,
+            'need_test_env': item_suite.need_test_env,
+            'test_env_desc': item_suite.test_env_desc
+        }
+        if test_type == 'performance':
+            res.update({
+                'need_test_suite_description': item_suite.need_test_suite_description,
+                'show_type': item_suite.show_type,
+            })
+        return res
+
+
+def get_template_func_item(tmp_is_default, template_id):
+    if tmp_is_default:
+        return [
+            {
+                "name": "测试项1",
+                "list": [
+                    {
+                        "test_tool": None,
+                        "suite_show_name": "TestSuite",
+                        "case_source": [
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                        ]
+                    }]
+            },
+            {
+                "name": "测试项2",
+                "list": [
+                    {
+                        "test_tool": None,
+                        "suite_show_name": "TestSuite",
+                        "case_source": [
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                        ]
+                    },
+                    {
+                        "test_tool": None,
+                        "suite_show_name": "TestSuite",
+                        "case_source": [
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                            {
+                                "test_conf_name": "TestConf"
+                            },
+                        ]
+                    }
+                ]
+            }
+        ]
+    return get_item_data_list(template_id, test_type='functional')
+
+
+def get_template_perf_item(tmp_is_default, template_id):
+    if tmp_is_default:
+        return [
+            {
+                "name": "测试项1",
+                "list": [
+                    {
+                        "test_tool": None,
+                        "suite_show_name": "TestSuite",
+                        "case_source": [
+                            {
+                                "test_conf_name": "TestConf",
+                                "metric_list": ["Metric", "Metric", "Metric"]
+                            },
+                            {
+                                "test_conf_name": "TestConf",
+                                "metric_list": ["Metric", "Metric", "Metric"]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    return get_item_data_list(template_id, test_type='performance')
+
+
+def get_item_data_list(tmpl_id, test_type):
+    item_data_list = list()
+    group_name_map = dict()
+    result = list()
+    item_data_map = dict()
+    for tmp_item in ReportTmplItem.objects.filter(tmpl_id=tmpl_id, test_type=test_type):
+        item_suite_list = list()
+        for item_suite in ReportTmplItemSuite.objects.filter(report_tmpl_item_id=tmp_item.id):
+            case_source = [{'test_conf_id': case_id, 'test_conf_name': case_name,
+                            'metric_list': list(TestMetric.objects.filter(
+                                object_type='case', object_id=case_id).values_list('name', flat=True))
+                            } for case_id, case_name in
+                           TestCase.objects.filter(id__in=item_suite.test_conf_list).values_list('id', 'name')]
+            tmp_suite = TestSuite.objects.filter(id=item_suite.test_suite_id).first()
+            suite_doc = None
+            if tmp_suite and tmp_suite.doc and tmp_suite.doc.find('Description') > -1 and \
+                    tmp_suite.doc.find('## Homepage') > -1:
+                suite_doc = tmp_suite.doc.split('Description')[1].split('## Homepage')[0]
+            item_suite_data = {
+                'test_suite_id': item_suite.test_suite_id,
+                'test_tool': suite_doc,
+                'suite_show_name': item_suite.test_suite_show_name,
+                'case_source': case_source,
+                # 'need_test_suite_description': item_suite.need_test_suite_description,
+            }
+            item_suite_list.append(item_suite_data)
+        item_data_list.append({
+            'name': tmp_item.name,
+            'test_suite_list': item_suite_list
+        })
+        item_data_map[tmp_item.name] = item_suite_list
+        if ':' in tmp_item.name:
+            item_name = tmp_item.name.split(':')[-1]
+            group_names = ':'.join(tmp_item.name.split(':')[:-1])
+            item_data = {
+                'name': item_name,
+                'list': item_suite_list
+            }
+            if group_names not in group_name_map:
+                group_name_map[group_names] = [item_data]
+            else:
+                group_name_map[group_names].append(item_data)
+        else:
+            result.append({
+                'name': tmp_item.name,
+                'list': item_suite_list
+            })
+    return pack_item_data(item_data_map)
+
+
+def pack_group_name(item_name_list, pack_data):
+    if len(item_name_list) > 1:
+        if 'group_data' in pack_data:
+            pack_data['group_data'] = {'group_name': item_name_list[0],
+                                       'group_data': None}
+        else:
+            pack_data = {'group_name': item_name_list[0],
+                         'group_data': {}}
+        return self.pack_group_name(item_name_list[1:], pack_data)
+    else:
+        if 'group_data' in pack_data:
+            pack_data['group_data'] = {'group_name': item_name_list[0],
+                                       'group_data': {'item_name': item_name_list[0],
+                                                      'test_suite_list': []}}
+        else:
+            pack_data = {'name': item_name_list[0],
+                         'test_suite_list': []}
+        return pack_data
+
+
+def trans_data(group_name_map):
+    result = list()
+    parent_dic = {}
+    for group_name in group_name_map.keys():
+        # 多级分组, 目前支持到三级
+        if ':' in group_name:
+            parent_name = group_name.split(':')[0]
+            son_name = ':'.join(group_name.split(':')[1:])
+            if parent_name in parent_dic:
+                parent_dic[parent_name].append({
+                    son_name: group_name_map[group_name]
+                })
+            else:
+                parent_dic[parent_name] = [{
+                    son_name: group_name_map[group_name]
+                }]
+        else:
+            result.append({
+                'name': group_name,
+                'is_group': True,
+                'list': group_name_map[group_name]
+            })
+    for tmp in parent_dic:
+        tmp_dict = {'name': tmp,
+                    'is_group': True,
+                    'list': []}
+        son_list = parent_dic[tmp]
+        for son_tmp in son_list:
+            son_name = list(son_tmp.keys())[0]
+            tmp_dict['list'].append({
+                'name': son_name,
+                'is_group': True,
+                'list': son_tmp[son_name]
+            })
+        result.append(tmp_dict)
+    return result
+
+
+def init_list(son_list, new_list):
+    for son in son_list:
+        if son.son_list:
+            new_list.append({
+                'name': son.name,
+                'is_group': True,
+                'list': init_list(son.son_list, [])
+            })
+        else:
+            new_list.append({
+                'name': son.name,
+                'list': son.item
+            })
+    return new_list
+
+
+def pack_item_data(item_data_map):
+    class Node:
+        def __init__(self, name, item):
+            self.son_list = list()
+            self.name = name
+            self.item = item
+
+        def add_son(self, son):
+            self.son_list.append(son)
+
+    def pack_root(origin_data, parent):
+        next_name = list()
+        for tmp_data in origin_data:
+            if ':' in tmp_data:
+                first = tmp_data.split(':')[0]
+                next_name.append(first)
+        next_data = {name: {} for name in next_name}
+        for key_name, key_value in origin_data.items():
+            if ':' in key_name:
+                first, res = key_name.split(':')[0], key_name.split(':')[1:]
+                next_data[first][':'.join(res)] = key_value
+            else:
+                son = Node(key_name, key_value)
+                parent.add_son(son)
+        for tmp_name in next_data:
+            son = Node(tmp_name, next_data[tmp_name])
+            parent.add_son(son)
+            pack_root(next_data[tmp_name], son)
+    root = Node('root', None)
+    pack_root(item_data_map, root)
+    result = list()
+    return init_list(root.son_list, result)
+
+
+def get_template_conf(tmpl_id, test_type):
+    item_id_list = ReportTmplItem.objects.filter(tmpl_id=tmpl_id, test_type=test_type).values_list('id')
+    item_suite = ReportTmplItemSuite.objects.filter(report_tmpl_item_id__in=item_id_list).first()
+    if item_suite is not None:
+        res = {
+            'need_test_description': item_suite.need_test_description,
+            'test_description_desc': item_suite.test_description_desc,
+            'need_test_conclusion': item_suite.need_test_conclusion,
+            'test_conclusion_desc': item_suite.test_conclusion_desc,
+            'need_test_env': item_suite.need_test_env,
+            'test_env_desc': item_suite.test_env_desc
+        }
+        if test_type == 'performance':
+            res.update({
+                'need_test_suite_description': item_suite.need_test_suite_description,
+                'show_type': item_suite.show_type,
+            })
+        return res
