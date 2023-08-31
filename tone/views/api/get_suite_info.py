96a182cb08344edd2b6a9eeba60862fbf40d3729
张学峰 <wb-zxf699644@alibaba-inc.com>
2023-08-31 14:07:52 +0800

    fix: add permission for suite case pub api

--- tone/views/api/get_suite_info.py
+++ tone/views/api/get_suite_info.py
@@ -5,12 +5,15 @@ from tone.core.utils.helper import CommResp
 from tone.core.common.verify_token import token_required
 from tone.core.common.expection_handler.error_catch import api_catch_error
 from datetime import datetime
-from tone.core.handle.report_handle import ReportHandle
+from tone.core.utils.permission_manage import check_admin_operator_permission
+from tone.core.common.expection_handler.error_code import ErrorCode
 
 
 @api_catch_error
 @token_required
 def get_suite_list(request):
+    if not check_admin_operator_permission(request.GET.get('username', None)):
+        assert None, ValueError(ErrorCode.PERMISSION_ERROR)
     resp = CommResp()
     serialize_flag, queryset = WorkspaceRetrieveService.filter(WorkspaceCaseRelation.objects.all(), request.GET)
     resp.data = queryset
@@ -20,6 +23,8 @@ def get_suite_list(request):
 @api_catch_error
 @token_required
 def get_case_list(request):
+    if not check_admin_operator_permission(request.GET.get('username', None)):
+        assert None, ValueError(ErrorCode.PERMISSION_ERROR)
     resp = CommResp()
     queryset = TestCaseService.filter(TestCase.objects.all(), request.GET)
     case_data_list = [{'id': case_info.id, 'name': case_info.name,
@@ -32,6 +37,8 @@ def get_case_list(request):
 @api_catch_error
 @token_required
 def get_metric_list(request):
+    if not check_admin_operator_permission(request.GET.get('username', None)):
+        assert None, ValueError(ErrorCode.PERMISSION_ERROR)
     resp = CommResp()
     queryset = TestMetricService.filter(TestMetric.objects.all(), request.GET)
     metric_data_list = [{'id': metric.id, 'name': metric.name} for metric in queryset]
@@ -39,9 +46,11 @@ def get_metric_list(request):
     return resp.json_resp()
 
 
-# @api_catch_error
-# @token_required
+@api_catch_error
+@token_required
 def get_suite_increase(request):
+    if not check_admin_operator_permission(request.GET.get('username', None)):
+        assert None, ValueError(ErrorCode.PERMISSION_ERROR)
     resp = CommResp()
     last_sync_time = request.GET['last_sync_time']
     queryset = TestCase.objects.filter(
@@ -64,8 +73,3 @@ def get_suite_increase(request):
         values_list('id', flat=True)
     resp.data = {'increase': suite_data_list, 'deleted': list(queryset_deleted)}
     return resp.json_resp()
-
-
-def reset_report(request):
-    job_id = request.GET['job_id']
-    ReportHandle(job_id).save_report()
