280eba986528a828225702f338bb4d89603643a2
张学峰 <wb-zxf699644@alibaba-inc.com>
2024-03-29 16:18:19 +0800

    feature: add log type for job keep tag

--- tone/core/common/operation_module/machine_management.py
+++ tone/core/common/operation_module/machine_management.py
@@ -12,7 +12,10 @@ OPERATION_OBJECT = {
     'machine_cluster_aliyun': '机器管理-云上集群',
     'machine_cluster_aligroup_server': '机器管理-集团集群-机器',
     'machine_cluster_aliyun_server': '机器管理-云上集群-机器',
-    'machine_server_tag': '机器管理-调度标签'
+    'machine_server_tag': '机器管理-调度标签',
+    'keep_three_months': 'job标签-保留三个月',
+    'keep_six_months': 'job标签-保留六个月',
+    'keep_one_year': 'job标签-保留一年'
 }
 
 OPERATION_OBJECT_LIST = list(OPERATION_OBJECT.keys())
