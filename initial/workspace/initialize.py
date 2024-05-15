977e6cc984ff3192ba2e721a32d3b3b5967ec927
张学峰 <wb-zxf699644@alibaba-inc.com>
2024-05-15 17:26:16 +0800

    fix: init keep tags

--- initial/workspace/initialize.py
+++ initial/workspace/initialize.py
@@ -2,7 +2,8 @@ from django.db import transaction
 
 import uuid
 from tone.core.utils.short_uuid import short_uuid
-from tone.models import Workspace, User, RoleMember, Role
+from django_q.models import Schedule
+from tone.models import Workspace, User, RoleMember, Role, JobTag
 from tone.services.sys.workspace_services import WorkspaceService
 
 
@@ -35,3 +36,30 @@ class WorkspaceDataInitialize(object):
             )
 
             WorkspaceService().add_workspace_relation_data(ws_id, system_user.id, first_init=True)
+
+    def initialize_keep_job_tags(self):
+        Schedule.objects.create(
+            name='clear_timeout_job',
+            func='tone.core.schedule.schedule_job.clear_timeout_job',
+            schedule_type=Schedule.MINUTES,
+            minutes=60
+        )
+        for ws in Workspace.objects.all():
+            JobTag.objects.create(
+                name='keep_three_months',
+                source_tag='system_tag',
+                ws_id=ws.id,
+                description='保留三个月'
+            )
+            JobTag.objects.create(
+                name='keep_six_months',
+                source_tag='system_tag',
+                ws_id=ws.id,
+                description='保留六个月'
+            )
+            JobTag.objects.create(
+                name='keep_one_year',
+                source_tag='system_tag',
+                ws_id=ws.id,
+                description='保留一年'
+            )
